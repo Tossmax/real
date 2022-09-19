79daf2d5b39f65a7a5b273553235574ef88dd26c
Ahmed <tossmax61@gmail.com>
2022-09-19 02:38:22 -0700

    Adding-products-to-the-page

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 1. Create React App
 2. Create Git Repository
+
+3. List Products
+4. create products array
+5. add product images
+6. render products
+7. style products
--- max/src/App.js
+++ max/src/App.js
@@ -1,10 +1,54 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">MAX</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  style={{
+                    height: '200px',
+                    width: '300px',
+                    justifyContent: 'center',
+                    borderRadius: '14px 14px 0 0',
+                  }}
+                />
+              </a>
+              <a href={`/product/${product.slug}`}>
+                <p style={{ fontSize: '15px', paddingLeft: '10px' }}>
+                  {product.name}
+                </p>
+              </a>
+              <p
+                style={{
+                  fontSize: '20px',
+                  fontWeight: '600',
+                  paddingLeft: '10px',
+                }}
+              >
+                ${product.price}
+              </p>
+              <button
+                style={{
+                  padding: '5px',
+                  margin: '1rem',
+                }}
+              >
+                Add to cart
+              </button>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
